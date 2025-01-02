3183ee195bf7e456a0ddf438ca2892cab815a12f
Mykola <jacktyler2391@gmail.com>
2025-01-02 15:12:23 +0200

    Add custom permissions to book-service-api
    Add custom permissions so that only admin is able to create/update/delete books, but users can only view available books (independent of wheter the user is authenticated)

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 
+from books import permissions
 from books.models import Book
 from books.serializers import BookSerializer, BookListSerializers, BookDetailSerializer
 
@@ -7,6 +8,7 @@ from books.serializers import BookSerializer, BookListSerializers, BookDetailSer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (permissions.IsAdminOrReadOnly,)
 
     def get_serializer_class(self):
         if self.action == 'list':
